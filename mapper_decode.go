26fe59817ac590a7005c8593d5f83b7b5596fb94
Sam Day <me@samcday.com>
2017-05-13 17:25:19 -0700

    mapper: split decoding/encoding into different files for sanity

---
+++ mapper_decode.go
@@ -0,0 +1,124 @@
+package rmarsh
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type decoderFunc func(p *Parser, v reflect.Value) error
+
+func (m *Mapper) valueDecoder(v reflect.Value) decoderFunc {
+	return m.typeDecoder(v.Type())
+}
+
+func (m *Mapper) typeDecoder(t reflect.Type) decoderFunc {
+	m.decLock.RLock()
+	dec := m.decCache[t]
+	m.decLock.RUnlock()
+	if dec != nil {
+		return dec
+	}
+
+	m.decLock.Lock()
+	defer m.decLock.Unlock()
+	if m.decCache == nil {
+		m.decCache = make(map[reflect.Type]decoderFunc)
+	}
+	m.decCache[t] = newTypeDecoder(t)
+	return m.decCache[t]
+}
+
+func newTypeDecoder(t reflect.Type) decoderFunc {
+	switch t.Kind() {
+	case reflect.Bool:
+		return boolDecoder
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return intDecoder
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return uintDecoder
+	case reflect.Ptr:
+		return newPtrDecoder(t)
+	}
+	return unsupportedTypeDecoder
+}
+
+func boolDecoder(p *Parser, v reflect.Value) error {
+	tok, err := p.Next()
+	if err != nil {
+		return err
+	}
+
+	switch tok {
+	case TokenTrue, TokenFalse:
+		v.SetBool(tok == TokenTrue)
+		return nil
+	// TODO: support other types and coerce them to something bool-y?
+	default:
+		// TODO: build a path
+		return fmt.Errorf("Unexpected token %v encountered while decoding bool", tok)
+	}
+}
+func intDecoder(p *Parser, v reflect.Value) error {
+	tok, err := p.Next()
+	if err != nil {
+		return err
+	}
+
+	switch tok {
+	case TokenFixnum:
+		n, err := p.Int()
+		if err != nil {
+			return err
+		}
+		if v.OverflowInt(n) {
+			return fmt.Errorf("Decoded int %d exceeds maximum width of %s", v.Type())
+		}
+		v.SetInt(n)
+		return nil
+	default:
+		return fmt.Errorf("Unexpected token %v encountered while decoding int", tok)
+	}
+}
+
+func uintDecoder(p *Parser, v reflect.Value) error {
+	tok, err := p.Next()
+	if err != nil {
+		return err
+	}
+
+	switch tok {
+	case TokenFixnum:
+		n, err := p.Int()
+		if err != nil {
+			return err
+		}
+		un := uint64(n)
+		if v.OverflowUint(un) {
+			return fmt.Errorf("Decoded int %d exceeds maximum width of %s", v.Type())
+		}
+		v.SetUint(un)
+		return nil
+	default:
+		return fmt.Errorf("Unexpected token %v encountered while decoding uint", tok)
+	}
+}
+
+type ptrDecoder struct {
+	elemDec decoderFunc
+}
+
+func (d *ptrDecoder) decode(p *Parser, v reflect.Value) error {
+	if v.IsNil() {
+		v.Set(reflect.New(v.Type().Elem()))
+	}
+	return d.elemDec(p, v.Elem())
+}
+
+func newPtrDecoder(t reflect.Type) decoderFunc {
+	dec := &ptrDecoder{newTypeDecoder(t.Elem())}
+	return dec.decode
+}
+
+func unsupportedTypeDecoder(p *Parser, v reflect.Value) error {
+	return fmt.Errorf("unsupported type %s", v.Type())
+}
---
+++ mapper_encode.go
@@ -0,0 +1,76 @@
+package rmarsh
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type encoderFunc func(gen *Generator, v reflect.Value) error
+
+func (m *Mapper) valueEncoder(v reflect.Value) encoderFunc {
+	return m.typeEncoder(v.Type())
+}
+
+func (m *Mapper) typeEncoder(t reflect.Type) encoderFunc {
+	m.encLock.RLock()
+	enc := m.encCache[t]
+	m.encLock.RUnlock()
+	if enc != nil {
+		return enc
+	}
+
+	m.encLock.Lock()
+	defer m.encLock.Unlock()
+	if m.encCache == nil {
+		m.encCache = make(map[reflect.Type]encoderFunc)
+	}
+	m.encCache[t] = newTypeEncoder(t)
+	return m.encCache[t]
+}
+
+func newTypeEncoder(t reflect.Type) encoderFunc {
+	switch t.Kind() {
+	case reflect.Bool:
+		return boolEncoder
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return intEncoder
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return uintEncoder
+	case reflect.Ptr:
+		return newPtrEncoder(t)
+	}
+	return unsupportedTypeEncoder
+}
+
+func boolEncoder(gen *Generator, v reflect.Value) error {
+	return gen.Bool(v.Bool())
+}
+
+func intEncoder(gen *Generator, v reflect.Value) error {
+	return gen.Fixnum(v.Int())
+}
+
+func uintEncoder(gen *Generator, v reflect.Value) error {
+	// TODO: properly detect overflow of signed 64bit int size and use a Bignum in that case
+	return gen.Fixnum(int64(v.Uint()))
+}
+
+func unsupportedTypeEncoder(gen *Generator, v reflect.Value) error {
+	return fmt.Errorf("unsupported type %s", v.Type())
+}
+
+type ptrEncoder struct {
+	elemEnc encoderFunc
+}
+
+func (e *ptrEncoder) encode(gen *Generator, v reflect.Value) error {
+	if v.IsNil() {
+		return gen.Nil()
+	}
+	return e.elemEnc(gen, v.Elem())
+}
+
+func newPtrEncoder(t reflect.Type) encoderFunc {
+	enc := &ptrEncoder{newTypeEncoder(t.Elem())}
+	return enc.encode
+}
