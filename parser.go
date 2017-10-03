c4f98ab114c6c57e10a40db2e07ecebc8981022a
Sam Day <me@samcday.com>
2017-10-02 21:46:15 -0700

    parser: some utterly insane shit in the name of optimizations

--- parser.go
+++ parser.go
@@ -124,35 +124,85 @@ func (p *Parser) Reset(r io.Reader) {
 }
 
 func (p *Parser) Read() (tok Token, b []byte, num int, err error) {
-	// RUN THE STATE MACHINE
-	switch p.state {
-	// the initial state of a Parser expects to read 2-byte magic and then a top level value
-	case parserStateTopLevel:
-		if p.pos == 0 {
-			// We can safely pull up to 3 bytes immediately. 2 bytes for the magic and all top level values
-			// will be at least 1 byte large.
-			if p.buflen < 3 {
-				if err = p.fill(3 - p.buflen); err != nil {
-					return
-				}
-			}
-
-			if p.buf[p.pos] != 0x04 || p.buf[p.pos+1] != 0x08 {
-				err = p.parserError("Expected magic header 0x0408, got 0x%.4X", int16(p.buf[p.pos])<<8|int16(p.buf[p.pos+1]))
-				return
-			}
-			p.pos = 2
-		}
-
-		// Our next state is EOF.
-		// Unless we read something interesting below which pushes something onto the stack.
-		p.state = parserStateEOF
-
-	// our EOF state - once we're here we continue to transition to the same state
-	// and return the same token until the Parser is reset to initial state.
-	case parserStateEOF:
+	// Quick early bailout check here. If parser state is "parserStateEOF" then we can just
+	// return an EOF token and exit.
+	if p.state == parserStateEOF {
 		tok = TokenEOF
 		return
+	}
+
+	// Gets set to false after we run SM.
+	runSM := true
+
+	// READ BYTES IF NECESSARY
+	// Running the state machine can bail out back here if there's not enough data in the read buffer
+	// to transition to the next state.
+	// This code would be WAY less complicated if we just filled the buffer with method calls when needed...
+	// But that costs too many precious nanos.
+	needed := 0
+pullbytes:
+	if needed > 0 {
+		// TODO: port over the stack-based prefetch here.
+
+		from, to := p.buflen, p.buflen+needed
+
+		if to > p.bufcap {
+			// Overflowed our read buffer, allocate a new one double the current size, or the required size if it's larger.
+			p.bufcap = p.bufcap * 2
+			if p.bufcap < to {
+				p.bufcap = to
+			}
+			buf := make([]byte, p.bufcap)
+			copy(buf, p.buf[0:p.buflen])
+			p.buf = buf
+		}
+
+		p.buflen += needed
+
+		var n int
+		for from < to && err == nil {
+			n, err = p.r.Read(p.buf[from:to])
+			from += n
+		}
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+			return
+		} else if err != nil {
+			err = errors.Wrap(err, "fill")
+			return
+		}
+
+		needed = 0
+	}
+
+	// RUN THE STATE MACHINE
+	if runSM {
+		switch p.state {
+		// the initial state of a Parser expects to read 2-byte magic and then a top level value
+		case parserStateTopLevel:
+			if p.pos == 0 {
+				// We can safely pull up to 3 bytes immediately. 2 bytes for the magic and all top level values
+				// will be at least 1 byte large.
+				if p.buflen < 3 {
+					needed = 3 - p.buflen
+					goto pullbytes
+				}
+
+				if p.buf[p.pos] != 0x04 || p.buf[p.pos+1] != 0x08 {
+					err = p.parserError("Expected magic header 0x0408, got 0x%.4X", int16(p.buf[p.pos])<<8|int16(p.buf[p.pos+1]))
+					return
+				}
+				p.pos = 2
+			}
+
+			// Our next state is EOF.
+			// Unless we read something interesting below which pushes something onto the stack.
+			p.state = parserStateEOF
+		}
+
+		// Now that we've run the SM, we don't want to run it again if the stream reads
+		// need to go back to pullbytes
+		runSM = false
 	}
 
 	// READ SOMETHING FROM THE STREAM
@@ -160,15 +210,12 @@ func (p *Parser) Read() (tok Token, b []byte, num int, err error) {
 		// This is the worst possible situation to be in - we have to go to the io.Reader to pull a single byte.
 		// This situation shouldn't occur very often on real world streams - as we should usually have enough to context to
 		// be doing safe read aheads.
-		err = p.fill(1)
-		if err != nil {
-			err = errors.Wrap(err, "error reading next token")
-			return
-		}
+		needed = 1
+		goto pullbytes
 	}
 
 	typ := p.buf[p.pos]
-	p.pos++
+	rd := 1
 
 	switch typ {
 	case typeNil:
@@ -182,106 +229,149 @@ func (p *Parser) Read() (tok Token, b []byte, num int, err error) {
 
 	case typeFixnum:
 		tok = TokenFixnum
-		num, err = p.long()
+		// num, err = p.long()
+
+		if p.pos+rd+1 > p.buflen {
+			needed = 1
+			goto pullbytes
+		}
+		rd++
+
+		var sz int
+		num, sz = p.readLongByte(p.buf[p.pos+1])
+		if sz > 0 && p.pos+rd+sz > p.buflen {
+			needed = p.pos + rd + sz - p.buflen
+			goto pullbytes
+		} else if sz > 0 {
+			for i := 0; i < sz; i++ {
+				if num < 0 {
+					num &= ^(0xff << uint(8*i))
+				}
+
+				num |= int(p.buf[p.pos+rd]) << uint(8*i)
+				rd++
+			}
+		}
 	}
+
+	p.pos += rd
 
 	return
 }
 
-func (p *Parser) long() (n int, err error) {
-	if p.pos == p.buflen {
-		err = p.fill(1)
-		if err != nil {
-			err = errors.Wrap(err, "error parsing long")
-			return
-		}
-	}
-
-	n = int(int8(p.buf[p.pos]))
-	p.pos++
-	if n == 0 {
-		return
-	} else if 4 < n && n < 128 {
+// Given an encoded byte, determines if it represents a concrete long or the number of bytes
+// needed to decode the real long.
+func (p *Parser) readLongByte(b byte) (n int, sz int) {
+	n = int(int8(b))
+	if 4 < n && n < 128 {
 		n = n - 5
-		return
 	} else if -129 < n && n < -4 {
 		n = n + 5
-		return
-	}
-
-	// It's a multibyte positive/negative num.
-	var sz int
-	if n > 0 {
+	} else if n > 0 {
 		sz = n
 		n = 0
-	} else {
+	} else if n != 0 {
 		sz = -n
 		n = -1
 	}
 
-	if p.pos+sz > p.buflen {
-		err = p.fill(p.pos + sz - p.buflen)
-		if err != nil {
-			err = errors.Wrap(err, "error parsing long")
-			return
-		}
-	}
-
-	for i := 0; i < sz; i++ {
-		if n < 0 {
-			n &= ^(0xff << uint(8*i))
-		}
-
-		n |= int(p.buf[p.pos]) << uint(8*i)
-		p.pos++
-	}
-
 	return
 }
+
+// func (p *Parser) long() (n int, err error) {
+// 	if p.pos == p.buflen {
+// 		err = p.fill(1)
+// 		if err != nil {
+// 			err = errors.Wrap(err, "error parsing long")
+// 			return
+// 		}
+// 	}
+
+// 	n = int(int8(p.buf[p.pos]))
+// 	p.pos++
+// 	if n == 0 {
+// 		return
+// 	} else if 4 < n && n < 128 {
+// 		n = n - 5
+// 		return
+// 	} else if -129 < n && n < -4 {
+// 		n = n + 5
+// 		return
+// 	}
+
+// 	// It's a multibyte positive/negative num.
+// 	var sz int
+// 	if n > 0 {
+// 		sz = n
+// 		n = 0
+// 	} else {
+// 		sz = -n
+// 		n = -1
+// 	}
+
+// 	if p.pos+sz > p.buflen {
+// 		err = p.fill(p.pos + sz - p.buflen)
+// 		if err != nil {
+// 			err = errors.Wrap(err, "error parsing long")
+// 			return
+// 		}
+// 	}
+
+// 	for i := 0; i < sz; i++ {
+// 		if n < 0 {
+// 			n &= ^(0xff << uint(8*i))
+// 		}
+
+// 		n |= int(p.buf[p.pos]) << uint(8*i)
+// 		p.pos++
+// 	}
+
+// 	return
+// }
 
 // pull bytes from the io.Reader into our read buffer
-func (p *Parser) fill(num int) (err error) {
-	// We don't do actual reads in sub Parser, the data is already in the buffer.
-	// if p.lnkID > -1 {
-	// 	return errors.Errorf("fill() called on replay Parser")
-	// }
+// func (p *Parser) fill(num int) (err error) {
+// 	// We don't do actual reads in sub Parser, the data is already in the buffer.
+// 	// if p.lnkID > -1 {
+// 	// 	return errors.Errorf("fill() called on replay Parser")
+// 	// }
 
-	// Whenever we go to pull bytes from the source, we prefetch as much as possible. We do this by examining the current
-	// stack. For example if we're processing an array with 500 elements and we've currently parsing element 232, then we
-	// know there's at *least* 267 bytes to come (even if every following element was just a single nil byte).
-	for i := len(p.stack) - 1; i >= 0; i-- {
-		if n := p.stack[i].sz - p.stack[i].pos - 1; n > 0 {
-			num += n
-		}
-	}
+// 	// Whenever we go to pull bytes from the source, we prefetch as much as possible. We do this by examining the current
+// 	// stack. For example if we're processing an array with 500 elements and we've currently parsing element 232, then we
+// 	// know there's at *least* 267 bytes to come (even if every following element was just a single nil byte).
+// 	for i := len(p.stack) - 1; i >= 0; i-- {
+// 		if n := p.stack[i].sz - p.stack[i].pos - 1; n > 0 {
+// 			num += n
+// 		}
+// 	}
 
-	from, to := p.buflen, p.buflen+num
+// 	from, to := p.buflen, p.buflen+num
 
-	if to > p.bufcap {
-		// Overflowed our read buffer, allocate a new one double the current size, or the required size if it's larger.
-		p.bufcap = p.bufcap * 2
-		if p.bufcap < to {
-			p.bufcap = to
-		}
-		buf := make([]byte, p.bufcap)
-		copy(buf, p.buf[0:p.buflen])
-		p.buf = buf
-	}
+// 	if to > p.bufcap {
+// 		// Overflowed our read buffer, allocate a new one double the current size, or the required size if it's larger.
+// 		p.bufcap = p.bufcap * 2
+// 		if p.bufcap < to {
+// 			p.bufcap = to
+// 		}
+// 		buf := make([]byte, p.bufcap)
+// 		copy(buf, p.buf[0:p.buflen])
+// 		p.buf = buf
+// 	}
 
-	p.buflen += num
+// 	p.buflen += num
 
-	var n int
-	for from < to && err == nil {
-		n, err = p.r.Read(p.buf[from:to])
-		from += n
-	}
-	if err == io.EOF {
-		err = io.ErrUnexpectedEOF
-	} else if err != nil {
-		err = errors.Wrap(err, "fill")
-	}
-	return
-}
+// 	var n int
+// 	for from < to && err == nil {
+// 		n, err = p.r.Read(p.buf[from:to])
+// 		from += n
+// 	}
+// 	if err == io.EOF {
+// 		err = io.ErrUnexpectedEOF
+// 	} else if err != nil {
+// 		err = errors.Wrap(err, "fill")
+// 	}
+// 	return
+// }
 
 // Constructs a ParserError using the current pos of the Parser.
 func (p *Parser) parserError(format string, a ...interface{}) ParserError {
